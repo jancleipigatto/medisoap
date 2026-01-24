49a03aa9d6bfbbfadc2806fcbdbf778da76470da
Janclei Pigatto <jancleipigatto@gmail.com>
2026-01-24 14:36:54 +0000

    Manual code change

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -176,7 +176,7 @@ export default function History() {
                   {!searchTerm && (
                     <Link to={createPageUrl("NewAnamnesis")}>
                       <Button className="bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700">
-                        Criar Anamnese
+                        Meu primeiro atendimento 
                       </Button>
                     </Link>
                   )}
